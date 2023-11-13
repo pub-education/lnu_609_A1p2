3f8d5539942cb7c811af27c2577f226cc3680019
ChrisJohDev <chris@chrisjohannesson.com>
2023-11-13 13:58:27 +0100

    Test that app exists

--- app/src/test/java/sinking_ships/AppTest.java
+++ app/src/test/java/sinking_ships/AppTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test void appExists() {
         App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        assertNotNull(classUnderTest, "app should be a class");
     }
 }
