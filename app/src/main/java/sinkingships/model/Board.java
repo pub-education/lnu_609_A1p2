9c45099919155e06d626e8c920decb59fbf43962
ChrisJohDev <chris@chrisjohannesson.com>
2023-11-27 14:35:12 +0100

    GREEN: BoardTest getShips()

--- app/src/main/java/sinkingships/model/Board.java
+++ app/src/main/java/sinkingships/model/Board.java
@@ -6,9 +6,23 @@ import java.util.LinkedList;
  * Abstract class for a player.
  */
 public class Board {
+  private LinkedList<ShipType> ships;
+
+  public Board() {
+    ships = new LinkedList<>();
+    loadShips();
+  }
+
+  private void loadShips() {
+    ships.add(ShipType.CARRIER);
+    ships.add(ShipType.BATTLESHIP);
+    ships.add(ShipType.CRUISER);
+    ships.add(ShipType.SUBMARINE);
+    ships.add(ShipType.DESTROYER);
+  }
 
   public LinkedList<ShipType> getShips() {
-    return new LinkedList<ShipType>();
+    return this.ships;
   }
   
 }
