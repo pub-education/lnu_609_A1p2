39078a519c61108bf4dd9a1ab11e2b6d0bc07fda
ChrisJohDev <chris@chrisjohannesson.com>
2023-11-27 15:40:06 +0100

    RED: Ship test getShipType

--- app/src/test/java/sinkingships/model/BoardTest.java
+++ app/src/test/java/sinkingships/model/BoardTest.java
@@ -14,4 +14,15 @@ public class BoardTest {
     Board sut = new Board();
     assertEquals(5, sut.getShips().size());
   }
+
+  @Test
+  public void shouldHaveTheCorrectShipsAfterCreation() {
+    Board sut = new Board();
+    assertEquals(ShipType.CARRIER, sut.getShips().get(0));
+    assertEquals(ShipType.BATTLESHIP, sut.getShips().get(1));
+    assertEquals(ShipType.CRUISER, sut.getShips().get(2));
+    assertEquals(ShipType.SUBMARINE, sut.getShips().get(3));
+    assertEquals(ShipType.DESTROYER, sut.getShips().get(4));
+  }
+
 }
