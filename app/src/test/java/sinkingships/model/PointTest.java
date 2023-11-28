51cf2c28468f396aa76092e654d80a36ddc9eea8
ChrisJohDev <chris@chrisjohannesson.com>
2023-11-28 20:53:36 +0100

    REFACTOR: PointTest shouldReturnHorizontalValueIndex

--- app/src/test/java/sinkingships/model/PointTest.java
+++ app/src/test/java/sinkingships/model/PointTest.java
@@ -1,6 +1,7 @@
 package sinkingships.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +9,7 @@ import org.junit.jupiter.api.Test;
  * Unit test for the Point class.
  */
 public class PointTest {
-  
+
   @Test
   public void shouldReturnHorizontalValueA() {
     Point sut = new Point('a', 2);
@@ -29,7 +30,22 @@ public class PointTest {
 
   @Test
   public void shouldReturnHorizontalValueIndex() {
-    Point sut = new Point('a', 2);
-    assertEquals(0, sut.getHorizontalIndex());
+    char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    try {
+      for (int i = 0; i < alphabet.length; i++) {
+        Point sut = new Point(alphabet[i], 2);
+        assertEquals(alphabet[i], sut.getHorizontalPosition());
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+      assertFalse(true);
+    }
+  }
+
+  @Test
+  public void shouldReturnHorizontalValueIndexUppercase() {
+
+    Point sut = new Point('E', 2);
+    assertEquals(4, sut.getHorizontalIndex());
   }
 }
